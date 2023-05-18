e22493b0d500ffb481b92ce589fedca07dddcf20
RaW <akram.iiuc.ctg@gmail.com>
2023-05-18 20:14:39 +0600

    first commit

---
+++ tailwind.config.js
@@ -0,0 +1,30 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
+  daisyui: {
+    themes: [
+      {
+        mytheme: {
+          primary: "#661AE6",
+
+          secondary: "#D926AA",
+
+          accent: "#1FB2A5",
+
+          neutral: "#a3e635",
+
+          "base-100": "#f3f4f6",
+
+          info: "#3ABFF8",
+
+          success: "#36D399",
+
+          warning: "#FBBD23",
+
+          error: "#F87272",
+        },
+      },
+    ],
+  },
+  plugins: [require("daisyui")],
+};
