266a97d6dae97f6962980b8d2121d2fdf8603c11
RaW <akram.iiuc.ctg@gmail.com>
2023-05-20 12:25:13 +0600

    ShopByCategory added

---
+++ src/Component/Pages/Home/ShopByCategory/CategoryCard.jsx
@@ -0,0 +1,48 @@
+import { Link } from "react-router-dom";
+import { Rating } from "@smastrom/react-rating";
+import "@smastrom/react-rating/style.css";
+
+const CategoryCard = ({ item }) => {
+  const { category_name, img, name, price, rating } = item;
+  console.log(item);
+  return (
+    <Link to="/">
+      <div className="overflow-hidden relative transition duration-200 transform hover:-translate-y-2 rounded shadow-lg hover:shadow-2xl">
+        <img
+          src={img}
+          alt=""
+          className="object-fill w-full h-56 md:h-64 xl:h-80"
+        />
+
+        <div className="bg-black px-6 py-4 bg-opacity-75 opacity-0 hover:opacity-100 text-gray-300 absolute inset-0 transition-opacity duration-200 flex flex-col text-center">
+          <h1 className="text-3xl font-bold">{category_name}</h1>
+          <br />
+          <p className="text-2xl font-bold">Name: {name}</p>
+          <br />
+          <div className="flex items-center gap-2">
+            <div>
+              <p>
+                <strong>Rating : </strong>
+                {rating}
+              </p>
+            </div>
+            <Rating
+              style={{ maxWidth: 130 }}
+              value={Math.round({ rating })}
+              readOnly
+            />
+          </div>
+          <br />
+          <p className="text-xl font-bold">Price: {price}</p>
+          <p className="mt-auto">
+            <button className="btn btn-primary font-bold text-white">
+              View Details
+            </button>
+          </p>
+        </div>
+      </div>
+    </Link>
+  );
+};
+
+export default CategoryCard;
--- src/Component/Pages/Home/ShopByCategory/ShopByCategory.jsx
+++ src/Component/Pages/Home/ShopByCategory/ShopByCategory.jsx
@@ -1,10 +1,104 @@
+import { useEffect, useState } from "react";
+import { Tab, Tabs, TabList, TabPanel } from "react-tabs";
+import "react-tabs/style/react-tabs.css";
+import CategoryCard from "./CategoryCard";
 
 const ShopByCategory = () => {
-    return (
-        <div>
-            
-        </div>
-    );
+  const [data, setData] = useState([]);
+  const [text, setText] = useState("Marvel");
+
+  const handlTabBtn = (event) => {
+    const name = event.target.innerText;
+    setText(name);
+  };
+
+  useEffect(() => {
+    fetch(`http://localhost:5000/shopByCategory/${text}`)
+      .then((res) => res.json())
+      .then((data) => setData(data));
+  }, [text]);
+
+  const [activeTab, setActiveTab] = useState(0);
+  const handleTabClick = (index) => {
+    setActiveTab(index);
+    console.log(index);
+  };
+
+  return (
+    <div className="text-center container mx-auto">
+      <h1 className="text-5xl font-bold text-center mb-4 text-[#00425A]">
+        Shop by Categories
+      </h1>
+      <div className="divider"></div>
+      <Tabs selectedIndex={activeTab} onSelect={handleTabClick}>
+        <TabList className="bg-opacity-0 tabs-boxed">
+          <Tab
+            className={`tab text-xl text-black font-bold ${
+              activeTab === 0 ? "tab-active" : ""
+            }`}
+            onClick={handlTabBtn}
+          >
+            Marvel
+          </Tab>
+          <Tab
+            className={`tab text-xl text-black font-bold ${
+              activeTab === 1 ? "tab-active" : ""
+            }`}
+            onClick={handlTabBtn}
+          >
+            Avenger
+          </Tab>
+          <Tab
+            className={`tab text-xl text-black font-bold ${
+              activeTab === 2 ? "tab-active" : ""
+            }`}
+            onClick={handlTabBtn}
+          >
+            Transformer
+          </Tab>
+          <Tab
+            className={`tab text-xl text-black font-bold ${
+              activeTab === 3 ? "tab-active" : ""
+            }`}
+            onClick={handlTabBtn}
+          >
+            StarWar
+          </Tab>
+        </TabList>
+
+        <br />
+
+        <TabPanel>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-3 mt-8">
+            {data.map((item) => (
+              <CategoryCard key={item._id} item={item}></CategoryCard>
+            ))}
+          </div>
+        </TabPanel>
+        <TabPanel>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2 mt-8">
+            {data.map((item) => (
+              <CategoryCard key={item._id} item={item}></CategoryCard>
+            ))}
+          </div>
+        </TabPanel>
+        <TabPanel>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2 mt-8">
+            {data.map((item) => (
+              <CategoryCard key={item._id} item={item}></CategoryCard>
+            ))}
+          </div>
+        </TabPanel>
+        <TabPanel>
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-2 mt-8">
+            {data.map((item) => (
+              <CategoryCard key={item._id} item={item}></CategoryCard>
+            ))}
+          </div>
+        </TabPanel>
+      </Tabs>
+    </div>
+  );
 };
 
 export default ShopByCategory;
